5a7a4a35008747e09d0cb60a78ee6f03e121669e
Sung <slee1070@my.bcit.ca>
2023-04-10 01:07:50 -0700

    Add constants for board

---
+++ board/__init__.py
@@ -0,0 +1,37 @@
+ADJECTIVES = ["eerie", "majestic", "gloomy", "serene", "perilous", "ancient", "enchanted", "desolate", "lush",
+              "opulent", "grimy", "sacred", "haunted", "exotic", "hidden", "sprawling", "cozy", "sinister",
+              "vibrant", "tranquil"]
+
+PLACES = ["castle", "forest", "tavern", "dungeon", "mountain", "swamp", "temple", "desert", "farm", "lake", "library",
+          "port", "mine", "plain", "glacier", "ruins", "tower"]
+
+SHOP_INVENTORY = {
+            "Weak ring of power": {
+                "cost": 30,
+                "bonus": {
+                    "stat": "attack",
+                    "amount": 5
+                }
+            },
+            "Weak amulet of vitality": {
+                "cost": 30,
+                "bonus": {
+                    "stat": "hp",
+                    "amount": 15
+                }
+            },
+            "Greater ring of power": {
+                "cost": 300,
+                "bonus": {
+                    "stat": "attack",
+                    "amount": 30
+                }
+            },
+            "Greater amulet of vitality": {
+                "cost": 300,
+                "bonus": {
+                    "stat": "hp",
+                    "amount": 100
+                }
+            }
+        }
